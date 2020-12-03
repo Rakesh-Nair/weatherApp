8b3a9e00b2fd1ebbf734153be845de674097f48b
Rakesh Nair <Rakesh.Nair@accelya.local>
2020-12-03 19:55:54 +0530

    Added error handling

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const request = require('postman-request');
 
-const url = 'http://api.weatherstack.com/current?access_key=acdb8cb5a42cc086ccf69d43ed728430&query=Los%20Angeles&units=m';
+const url = 'http://api.weatherstack.com/current?access_key=acdb8cb5a42cc086ccf69d43ed728430&query=34.052,-118.243&units=m';
 
 request({ url: url, json: true }, (error, response) => {
-    const parsedBody = response.body.current;
-    console.log('It is currently ' + parsedBody.temperature + ' degrees but it feels like ' + parsedBody.feelslike + ' degrees');
+    if (error) {
+        console.log('Unable to connect to Weather service !');
+    }
+    else if (response.body.error) {
+        console.log(response.body.error.info);
+    }
+    else {
+        const parsedBody = response.body.current;
+        console.log('It is currently ' + parsedBody.temperature + ' degrees but it feels like ' + parsedBody.feelslike + ' degrees');
+
+    }
 })
 
 const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmFrZXNobmFpciIsImEiOiJja2k4ZXAyYTYwNTN3MzRuNHU2bDQwMzFuIn0.qywtm1t5SZpeNBSXguwUug&limit=1'
 
 request({ url: geoUrl, json: true }, (error, response) => {
-    const longitude = response.body.features[0].center[0];
-    const latitude = response.body.features[0].center[1];
-    console.log('latitude ' + latitude);
-    console.log('longitude ' + longitude);
+    if (error) {
+        console.log('Unable to connect to Location service !');
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to fetch the result');
+    }
+    else {
+        const longitude = response.body.features[0].center[0];
+        const latitude = response.body.features[0].center[1];
+        console.log('latitude ' + latitude);
+        console.log('longitude ' + longitude);
+    }
 })
